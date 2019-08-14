46d74f4064e767b398e305c85ad443ecd2c779fe
enfralys <enfralys>
2019-08-14 18:28:37 -0500

    fix

--- src/models/multimedia.js
+++ src/models/multimedia.js
@@ -4,7 +4,7 @@ const mysql = require('mysql')
 connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: 'Dev@2019_R1',
     database: 'backend'
 })
 
